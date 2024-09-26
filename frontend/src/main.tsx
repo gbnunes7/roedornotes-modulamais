89ffd0c724d2162309a694392a66d28fd541ee0c
Gabriel Melo <bielrj170@gmail.com>
2024-09-25 23:17:49 -0300

    feat: distribute provider to all routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './styles/index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./styles/index.css";
+import AppProvider from "./context/appContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<AppProvider>
+			<App />
+		</AppProvider>
+	</StrictMode>
+);
