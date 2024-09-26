04e08d3a056f46588594b8c33c00aadcfeb1f850
Gabriel Melo <bielrj170@gmail.com>
2024-09-25 21:54:08 -0300

    feat: use dotenv lib to hide db password

--- backend/src/database/config/config.js
+++ backend/src/database/config/config.js
@@ -1,21 +1,23 @@
+require("dotenv").config();
+
 module.exports = {
 	development: {
 		username: "root",
-		password: "Eusouasenha123@",
+		password: process.env.DB_PASSWORD,
 		database: "database_development",
 		host: "127.0.0.1",
 		dialect: "mysql",
 	},
 	test: {
 		username: "root",
-		password: "Eusouasenha123@",
+		password: process.env.DB_PASSWORD,
 		database: "database_test",
 		host: "127.0.0.1",
 		dialect: "mysql",
 	},
 	production: {
 		username: "root",
-		password: "Eusouasenha123@",
+		password: process.env.DB_PASSWORD,
 		database: "database_production",
 		host: "127.0.0.1",
 		dialect: "mysql",
