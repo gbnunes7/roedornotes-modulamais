49a13aecef8e257357fb2d9da25e13fc3bcc91e7
Gabriel Melo <bielrj170@gmail.com>
2024-09-26 16:32:25 -0300

    style: change name of app to approutes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import AppRoutes from "./routes/App.tsx";
 import "./styles/index.css";
-import AppProvider from "./context/appContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<AppProvider>
-			<App />
-		</AppProvider>
+		<AppRoutes />
 	</StrictMode>
 );
