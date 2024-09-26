e59b39685eaf4fb83747c3d009759e7510837a9c
Gabriel Melo <bielrj170@gmail.com>
2024-09-26 17:50:00 -0300

    refactor: add type and onclick props

--- frontend/src/components/button/index.tsx
+++ frontend/src/components/button/index.tsx
@@ -4,13 +4,19 @@ const Button = ({
 	children,
 	background,
 	color,
+	type,
+	onclick,
 }: {
 	background: string;
 	color: string;
 	children: React.ReactNode;
+	type?: "submit" | "reset" | "button";
+	onclick?: React.MouseEventHandler<HTMLButtonElement>;
 }) => {
 	return (
 		<button
+			type={type}
+			onClick={onclick}
 			className="py-2 px-6 w-[153px] rounded-[32px] text-[20px] text-center border border-[#041832] transition-transform duration-300 transform hover:scale-110"
 			style={{ backgroundColor: background, color: color }}
 		>
