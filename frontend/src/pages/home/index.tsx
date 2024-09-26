0ce8d443591ba7f5382d6deedf1cc810253a5347
Gabriel Melo <bielrj170@gmail.com>
2024-09-26 16:19:51 -0300

    feat: create a home page

---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+import Banner from "../../components/banner";
+import BannerCliqueAqui from "../../components/bannerCliqueAqui";
+import Header from "../../components/header";
+
+const Home = () => {
+	return (
+		<>
+			<Header />
+			<Banner />
+			<BannerCliqueAqui />
+		</>
+	);
+};
+
+export default Home
