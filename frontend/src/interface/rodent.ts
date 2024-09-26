04e404b5ec6ce6b6b5ff5c555eb35243369df8f9
Gabriel Melo <bielrj170@gmail.com>
2024-09-26 17:50:53 -0300

    feat: initialize type to states

--- frontend/src/interface/rodent.ts
+++ frontend/src/interface/rodent.ts
@@ -2,11 +2,21 @@ export interface rodentData {
 	id: Number;
 	nome: string;
 	especie: string;
-	idade: Number;
-	peso: Number;
+	idade: string;
+	peso: string;
 	status: string;
 	habitat: string;
 	comportamento: string;
 	dieta: string;
 	observacao: string;
 }
+
+export type NomeRodent = string;
+export type EspecieRodent = string;
+export type IdadeRodent = string;
+export type PesoRodent = string;
+export type StatusRodent = string;
+export type HabitatRodent = string;
+export type ComportamentoRodent = string;
+export type DietaRodent = string;
+export type ObservacaoRodent = string;
