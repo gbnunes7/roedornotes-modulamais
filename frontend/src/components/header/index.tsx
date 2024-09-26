6e56570f617ce42a82c28bbb9a1b4c9b04e74c6f
Gabriel Melo <bielrj170@gmail.com>
2024-09-26 20:52:36 -0300

    feat: add a link on img logo to navigate home

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom";
 import HamburgerMenu from "../menuHamburguer";
 
 const Header = () => {
 	return (
 		<header className="bg-blue-200 px-2 max-h-[120px] md:justify-between md:px-20 flex items-center justify-evenly gap-24 shadow shadow-black">
-			<img
-				src="/images/roedorNotes.png"
-				alt="Imagem da logo"
-				className="max-w-[200px]"
-			/>
+			<Link to={"/"}>
+				<img
+					src="/images/roedorNotes.png"
+					alt="Imagem da logo"
+					className="max-w-[200px]"
+				/>
+			</Link>
 
 			<HamburgerMenu />
 		</header>
